1239e0ec816cdca608740dfc6a048b6523ff2bcc
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-02-05 19:16:29 -0500

    Update blurred-hero.tsx

--- components/blurred-hero.tsx
+++ components/blurred-hero.tsx
@@ -40,10 +40,7 @@ export function BlurredHero({
 }: BlurredHeroProps) {
   return (
     <section
-      className={cn(
-        "relative isolate aspect-4/3 sm:aspect-video lg:aspect-21/9",
-        className,
-      )}
+      className={cn("relative isolate aspect-video lg:aspect-21/9", className)}
     >
       {/* Background is clipped, content can overflow (search dropdown) */}
       <div className="absolute inset-0 -z-10 overflow-hidden">
